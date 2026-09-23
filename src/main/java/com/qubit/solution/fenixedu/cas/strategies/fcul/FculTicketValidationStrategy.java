a8c32491327ce378f342485f761d9280f89af126
Diogo Godinho <diogo.godinho@qub-it.com>
2026-09-23 12:48:15 +0100

    Migrate from javaee to jakarta

--- src/main/java/com/qubit/solution/fenixedu/cas/strategies/fcul/FculTicketValidationStrategy.java
+++ src/main/java/com/qubit/solution/fenixedu/cas/strategies/fcul/FculTicketValidationStrategy.java
@@ -30,8 +30,8 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 import org.apache.commons.lang.StringUtils;
 import org.fenixedu.academic.domain.Person;
--- src/main/java/com/qubit/solution/fenixedu/cas/strategies/iscsp/ISCSPTicketValidationStrategy.java
+++ src/main/java/com/qubit/solution/fenixedu/cas/strategies/iscsp/ISCSPTicketValidationStrategy.java
@@ -28,8 +28,8 @@ package com.qubit.solution.fenixedu.cas.strategies.iscsp;
 
 import java.util.Map;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.lang.math.NumberUtils;
